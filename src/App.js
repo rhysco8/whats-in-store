f9f42bb92399eea2a84481ef9bbd65b3c7039bd8
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-09-11 19:54:40 +0100

    chore: create Table class and import it to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Table from './table.js';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>What's In Store</h1>
-    </div>
+    <Table />
   );
 }
 
