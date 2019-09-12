7c11e815c4d32c6e2633f493c1be97f070149622
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-09-12 14:59:54 +0100

    feat: display table of products

--- src/Table.js
+++ src/Table.js
@@ -4,18 +4,43 @@ class Table extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [
-        { name: 'Bluebell 3 seat sofa in Charcoal brushed linen cotton', sku: 'FSBLB130BLCCHA', quantity: 1 },
-        { name: 'Isla armchair in Ash soft wool', sku: 'FSISL110STWASH', quantity: 1 },
-        { name: 'Stella medium corner sofa in Olive cotton matt velvet', sku: 'FMSTE320CMVOLI', quantity: 1 }
+      products: [
+        { id: 1, name: 'Bluebell 3 seat sofa in Charcoal brushed linen cotton', sku: 'FSBLB130BLCCHA', quantity: 1 },
+        { id: 2, name: 'Isla armchair in Ash soft wool', sku: 'FSISL110STWASH', quantity: 1 },
+        { id: 3, name: 'Stella medium corner sofa in Olive cotton matt velvet', sku: 'FMSTE320CMVOLI', quantity: 1 }
       ]
     }
   }
 
+  renderTableData() {
+    return this.state.products.map((product, index) => {
+      const { id, name, sku, quantity } = product
+      return (
+        <tr key={id}>
+          <td>{id}</td>
+          <td>{name}</td>
+          <td>{sku}</td>
+          <td>{quantity}</td>
+        </tr>
+      )
+    });
+  }
+
   render() {
     return (
-      <div class="table table-hover">
-        <h2>What's In Store</h2>
+      <div>
+        <h1>What's In Store</h1>
+        <table id='products' class="table table-hover">
+          <thead>
+            <th>#</th>
+            <th>Product</th>
+            <th>SKU</th>
+            <th>Quantity</th>
+          </thead>
+          <tbody>
+            {this.renderTableData()}
+          </tbody>
+        </table>
       </div>
     )
   }
