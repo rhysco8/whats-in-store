86ae8429aba953f48a26f53cfcff43da5d9d4967
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-09-13 17:14:35 +0100

    test: renders table headers

---
+++ src/Table.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Table from './Table';
+
+it('renders name of app', () => {
+  const wrapper = shallow(<Table />);
+  const pageHeader = <h1>What's In Store</h1>;
+  expect(wrapper).toContainReact(pageHeader);
+});
+
+it('renders table headers', () => {
+  const wrapper = shallow(<Table />);
+  const tableHeaderId = <th>ID</th>;
+  const tableHeaderName = <th>NAME</th>;
+  const tableHeaderSku = <th>SKU</th>;
+  const tableHeaderQuantity = <th>QUANTITY</th>;
+
+  expect(wrapper).toContainReact(tableHeaderId);
+  expect(wrapper).toContainReact(tableHeaderName);
+  expect(wrapper).toContainReact(tableHeaderSku);
+  expect(wrapper).toContainReact(tableHeaderQuantity);
+});
