ce456fc3a28d3e4e222fc063764063b5296f93be
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-09-16 18:07:29 +0100

    test: pending - renders table headers

---
+++ src/Products.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Products } from './Products';
+
+xit('renders table headers', () => { // TODO: need to mock product list
+  const wrapper = shallow(<Products />);
+  const tableHeaderIndex = <th>#</th>;
+  const tableHeaderName = <th>NAME</th>;
+  const tableHeaderSku = <th>SKU</th>;
+  const tableHeaderQuantity = <th>QUANTITY</th>;
+
+  expect(wrapper).toContainReact(tableHeaderIndex);
+  expect(wrapper).toContainReact(tableHeaderName);
+  expect(wrapper).toContainReact(tableHeaderSku);
+  expect(wrapper).toContainReact(tableHeaderQuantity);
+});
