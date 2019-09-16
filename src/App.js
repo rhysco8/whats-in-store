1377e299cea5944c469334e0c90aa6a6112bfabe
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-09-16 17:48:21 +0100

    feat: render products using const rather than Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import Table from './Table.js';
+import React, { Component } from 'react';
+import Table from './Table'; // TODO: Remove this line
+import { Products } from './Products';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>What's In Store</h1>
-      <Table />
-    </div>
-  );
+class App extends Component {
+  products = () => {
+    let productList = [
+      { name: 'Snowdrop 3 seat sofa in Charcoal brushed linen cotton', sku: 'FSSNO130BLCCHA', quantity: 1 },
+      { name: 'Isla armchair in Ash soft wool', sku: 'FSISL110STWASH', quantity: 1 },
+      { name: 'Stella medium corner sofa in Olive cotton matt velvet', sku: 'FMSTE320CMVOLI', quantity: 1 }
+    ]
+    return productList
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>What's In Store</h1>
+        <Products products={this.products()} />
+      </div>
+    );
+  }
 }
 
 export default App;
