3e9945feec004cbbaa0e77d08607517f68075d15
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-10-02 13:40:01 +0100

    test: renders table data

--- src/test/Products.test.js
+++ src/test/Products.test.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Products } from '../Products';
 
-xit('renders table headers', () => { // TODO: need to mock product list
-  const wrapper = shallow(<Products />);
-  const tableHeaderIndex = <th>#</th>;
-  const tableHeaderName = <th>NAME</th>;
-  const tableHeaderSku = <th>SKU</th>;
-  const tableHeaderQuantity = <th>QUANTITY</th>;
+describe('Products component renders props', () => {
+  it('renders table headers', () => {
+    const productList = [];
+    const wrapper = shallow(<Products products={productList} />);
 
-  expect(wrapper).toContainReact(tableHeaderIndex);
-  expect(wrapper).toContainReact(tableHeaderName);
-  expect(wrapper).toContainReact(tableHeaderSku);
-  expect(wrapper).toContainReact(tableHeaderQuantity);
+    expect(wrapper).toContainReact(<th>#</th>);
+    expect(wrapper).toContainReact(<th>Description</th>);
+    expect(wrapper).toContainReact(<th>SKU</th>);
+    expect(wrapper).toContainReact(<th>Quantity</th>);
+  });
+
+  it('renders table data', () => {
+    const productList = [
+      { name: 'test sofa', sku: 'TEST', quantity: 2 }
+    ];
+    const wrapper = shallow(<Products products={productList} />);
+
+    expect(wrapper).toContainReact(<td>1</td>);
+    expect(wrapper).toContainReact(<td>test sofa</td>);
+    expect(wrapper).toContainReact(<td>TEST</td>);
+    expect(wrapper).toContainReact(<td>2</td>);
+  });
 });
